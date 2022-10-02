40bdbd0d69db704154951715e575f96308c174d6
aashish2604 <aashish.260401@gmail.com>
2022-10-02 23:17:28 +0530

    Added slashbox with borders

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:concetto_app/services/configs/size_config.dart';
+import 'package:concetto_app/widgets/slashbox.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,27 +7,37 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double height = SizeConfig.instance.screenHeight;
+    double screenHeight = SizeConfig.instance.screenHeight;
+    double screenWidth = SizeConfig.instance.screenWidth;
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: height * 0.06),
-            child: const Text('Concetto',
-                style: TextStyle(
-                    fontSize: 36.0,
-                    fontFamily: "orbitron",
-                    letterSpacing: 5.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white)),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          const Center(
-            child: Text('Home Page', style: TextStyle(color: Colors.white)),
-          ),
-        ],
+      body: SizedBox(
+        width: screenWidth,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: screenHeight * 0.06),
+              child: const Text('Concetto',
+                  style: TextStyle(
+                      fontSize: 36.0,
+                      fontFamily: "orbitron",
+                      letterSpacing: 5.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            BorderedSlashBox(
+              height: 200,
+              width: SizeConfig.instance.screenWidth * 0.8,
+              child: const Text(
+                'Something',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/slashbox.dart
@@ -0,0 +1,95 @@
+import 'package:concetto_app/services/theme/custom_colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BorderedSlashBox extends StatelessWidget {
+  final Widget? child;
+  final double height;
+  final double width;
+  const BorderedSlashBox(
+      {super.key, required this.height, required this.width, this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return SlashBox(
+      height: height,
+      width: width,
+      color: kBrightCyan,
+      child: Center(
+        child: SlashBox(
+          height: height - 4,
+          width: width - 4,
+          color: kCoolGrey,
+          child: Padding(
+            padding: EdgeInsets.only(
+                top: (height * 0.15 + 2), left: (width * 0.1 + 2)),
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SlashBox extends StatelessWidget {
+  final double height;
+  final double width;
+  final Widget? child;
+  final Color color;
+  const SlashBox(
+      {required this.height,
+      required this.width,
+      required this.color,
+      this.child,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: CustomClipPath(),
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          color: color,
+          // border: Border.all(color: kBrightCyan, width: 2),
+        ),
+        child: child,
+      ),
+    );
+  }
+}
+
+class CustomClipPath extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double w = size.width;
+    double h = size.height;
+
+    Path path = Path();
+    // path.moveTo(0, h * 0.1802192);
+    // path.lineTo(0, h * 0.9976932);
+    // path.lineTo(w * 0.8958333, h * 0.9976932);
+    // path.lineTo(w * 0.9983333, h * 0.8290081);
+    // path.lineTo(w * 0.9983333, h * 0.0028835);
+    // path.lineTo(w * 0.1041667, h * 0.0043253);
+    // path.lineTo(0, h * 0.1802192);
+    // path.close();
+    path.moveTo(0, h * 0.15);
+    path.lineTo(0, h);
+    path.lineTo(w * 0.85, h);
+    path.lineTo(w, h * 0.85);
+    path.lineTo(w, 0);
+    path.lineTo(w * 0.15, 0);
+    // path.lineTo(w, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
