f80c2aed52a91c446eafd6d37199bf5cc292f5ff
aashish2604 <aashish.260401@gmail.com>
2022-10-19 05:40:23 +0530

    Added exhibiton and workshop to home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,8 +4,12 @@ import 'dart:ui';
 
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:concetto_app/repository/events_repository.dart';
+import 'package:concetto_app/repository/exhibition_repository.dart';
+import 'package:concetto_app/repository/workshop_repository.dart';
 import 'package:concetto_app/screens/about_us.dart';
 import 'package:concetto_app/screens/events_guests/event_guest.dart';
+import 'package:concetto_app/screens/exhibitions/exhibitions.dart';
+import 'package:concetto_app/screens/workshop/workshop.dart';
 import 'package:concetto_app/services/configs/size_config.dart';
 import 'package:concetto_app/services/theme/custom_colors.dart';
 import 'package:concetto_app/widgets/navigation_drawer.dart';
@@ -62,6 +66,11 @@ class _HomeScreenState extends State<HomeScreen> {
 
     double screenHeight = SizeConfig.instance.screenHeight;
     double screenWidth = SizeConfig.instance.screenWidth;
+    TextStyle slashboxTextStyle = const TextStyle(
+      color: kBrightCyan,
+      fontSize: 22.0,
+      fontFamily: 'orbitron',
+    );
 
     return Container(
       decoration: const BoxDecoration(
@@ -111,7 +120,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   GestureDetector(
                     onTap: () {
-                      EventsRepository().getEvents();
                       Navigator.of(context).push(MaterialPageRoute(
                           builder: (context) => const EventsGuests()));
                     },
@@ -146,33 +154,76 @@ class _HomeScreenState extends State<HomeScreen> {
                   const SizedBox(
                     height: 40,
                   ),
-                  const Center(
-                    child: Text(
-                      'Coming Soon...',
-                      style: TextStyle(
-                          fontFamily: "orbitron",
-                          fontSize: 40,
-                          color: Colors.white),
-                      // style: TextStyle(color: Colors.white, fontSize: 40)
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Text(
-                    '$days:$hours:$minutes:$seconds',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.w400,
-                        color: kBrightCyan,
-                        fontSize: 40),
-                  ),
-                  const SizedBox(
-                    height: 60,
-                  ),
+                  // const Center(
+                  //   child: Text(
+                  //     'Coming Soon...',
+                  //     style: TextStyle(
+                  //         fontFamily: "orbitron",
+                  //         fontSize: 40,
+                  //         color: Colors.white),
+                  //     // style: TextStyle(color: Colors.white, fontSize: 40)
+                  //   ),
+                  // ),
+                  // const SizedBox(
+                  //   height: 20.0,
+                  // ),
+                  // Text(
+                  //   '$days:$hours:$minutes:$seconds',
+                  //   style: const TextStyle(
+                  //       fontWeight: FontWeight.w400,
+                  //       color: kBrightCyan,
+                  //       fontSize: 40),
+                  // ),
                   YoutubePlayerContainer(
                     backgroundColor: kCoolGrey,
                     width: screenWidth * 0.85,
                     height: screenHeight * 0.35,
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const ExhibitionList()));
+                        },
+                        child: BorderedSlashBox(
+                          backgroundImage:
+                              'assets/images/slashbox_dearkbackground.png',
+                          height: 150.0,
+                          width: SizeConfig.instance.screenWidth * 0.45,
+                          padding: EdgeInsets.zero,
+                          child: Center(
+                            child: Text(
+                              'Exhibitions',
+                              style: slashboxTextStyle,
+                            ),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const WorkshopList()));
+                        },
+                        child: BorderedSlashBox(
+                          backgroundImage:
+                              'assets/images/slashbox_dearkbackground.png',
+                          height: 150.0,
+                          width: SizeConfig.instance.screenWidth * 0.45,
+                          padding: EdgeInsets.zero,
+                          child: Center(
+                            child: Text(
+                              'Workshop',
+                              style: slashboxTextStyle,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                   const SizedBox(
                     height: 60,
